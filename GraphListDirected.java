4a00072f97c08cf65f1793aa309bff42c260c702
Jim Teresco <terescoj@gmail.com>
2025-01-26 08:23:34 -0500

    Start framework for adjacency list implementation

---
+++ GraphListDirected.java
@@ -0,0 +1,11 @@
+/**
+ * A simple directed graph structure using an adjacency list represenation.
+ * 
+ * @author Jim Teresco and copilot
+ * @version Spring 2025
+*/
+
+public class GraphListDirected {
+
+
+}
